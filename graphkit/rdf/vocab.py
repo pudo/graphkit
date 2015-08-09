199ea5044f5c1256b5b364d0af094cd0aaffd749
Friedrich Lindenberg <friedrich@pudo.org>
2015-08-09 19:28:56 +0200

    Clean up the popolo-ld imported code a bit more.

--- graphkit/rdf/vocab.py
+++ graphkit/rdf/vocab.py
@@ -1,18 +1,9 @@
 from uuid import uuid4
-from rdflib import Literal, URIRef, Namespace
+from rdflib import URIRef, Namespace
 
-POPOLO = Namespace('urn:popolo:')
+PRED = Namespace('p/')
 ID = Namespace('id:')
-BNODE = Namespace('b:')
-
-Person = URIRef('http://www.popoloproject.com/schemas/person.json#')
-Organization = URIRef('http://www.popoloproject.com/schemas/organization.json#')
-Membership = URIRef('http://www.popoloproject.com/schemas/membership.json#')
 
 
-def bind(graph):
-    graph.bind('p', POPOLO)
-
-
-def node():
-    return BNODE[uuid4().hex[:20]]
+def BNode():
+    return URIRef(uuid4().urn)
